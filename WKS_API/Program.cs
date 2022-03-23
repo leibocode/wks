e26f5aa52fcc2ee39159470fe336fbc6d0ffccef
leibocode <252517584@qq.com>
2022-03-23 16:11:40 +0800

    增加swagger

--- WKS_API/Program.cs
+++ WKS_API/Program.cs
@@ -18,12 +18,9 @@ builder.Services.AddCors(options => {
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("any");
 
